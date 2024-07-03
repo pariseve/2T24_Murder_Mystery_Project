0ce35ff6533371102f1436dc91efe70b97ce8ac0
anoobcarrot <136556154+anoobcarrot@users.noreply.github.com>
2024-07-03 14:56:37 +1000

    Revert Changes

--- Assets/Dialogue/Scripts/BoolManager.cs
+++ Assets/Dialogue/Scripts/BoolManager.cs
@@ -7,8 +7,6 @@ public class BoolManager : MonoBehaviour
 
     // Boolean variables
     public bool SheriffIanDay1Town1;
-    public bool EdithDay1Street;
-    public bool EdithDay1StreetFindRaymond;
 
     private void Awake()
     {
@@ -32,12 +30,6 @@ public class BoolManager : MonoBehaviour
             case "SheriffIanDay1Town1":
                 SheriffIanDay1Town1 = value;
                 break;
-            case "EdithDay1Street":
-                EdithDay1Street = value;
-                break;
-            case "EdithDay1StreetFindRaymond":
-                EdithDay1StreetFindRaymond = value;
-                break;
             // Add cases for additional boolean variables
             default:
                 Debug.LogWarning($"Bool '{boolName}' not found.");
@@ -52,10 +44,6 @@ public class BoolManager : MonoBehaviour
         {
             case "SheriffIanDay1Town1":
                 return SheriffIanDay1Town1;
-            case "EdithDay1Street":
-                return EdithDay1Street;
-            case "EdithDay1StreetFindRaymond":
-                return EdithDay1StreetFindRaymond;
             // Add cases for additional boolean variables
             default:
                 Debug.LogWarning($"Bool '{boolName}' not found.");
